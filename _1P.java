8093f24fe6573af04dc0214a720cbabe4ff1c6c9
KANITA VERMA <vermakanita13@gmail.com>
2024-10-09 23:55:37 +0530

    Add files via upload

---
+++ _1P.java
@@ -0,0 +1,19 @@
+/*
+*
+**
+***
+****
+    
+ */
+public class _1P {
+    public static void main(String [] args){
+     for (int i=0;i<5;i++){
+        for(int j=0;j<i;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+     }
+    }
+
+    
+}
