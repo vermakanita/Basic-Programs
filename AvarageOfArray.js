9af430bfc1ed63fe728dfd919824b0f6821d8bd3
KANITA VERMA <vermakanita13@gmail.com>
2024-12-04 21:50:56 +0530

    Add files via upload

---
+++ AvarageOfArray.js
@@ -0,0 +1,11 @@
+let a=[1,2,3,4];
+let x=a.length-1;
+let sum=0;
+for(let i=0;i<a.length;i++){
+    sum=sum+a[i];
+     
+}
+// console.log((sum/x));
+console.log(sum)
+console.log(a.length-1)
+console.log(sum/(a.length-1))
