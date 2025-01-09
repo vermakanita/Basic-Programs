a894a03a180d52aeeeec24a4c179facc4fd03161
KANITA VERMA <vermakanita13@gmail.com>
2025-01-09 20:59:41 +0530

    Add files via upload

---
+++ Q3.js
@@ -0,0 +1,18 @@
+  let n=5;
+  for(let i=1;i<=5;i++){
+    for(let j=0;j<i;j++){
+        process.stdout.write("*")
+    }
+    console.log()
+
+  }
+
+  for(let i=n-1;i>0;i--){
+    
+    
+    for(j=0;j<i;j++){
+        process.stdout.write("*")
+    }
+    console.log()
+
+  }
